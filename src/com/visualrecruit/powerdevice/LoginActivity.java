fc88dcb462e68c3be40eb2e63f3afa5073fdaa3b
Damien Cooke <damien@smartphonedev.com>
2014-07-28 21:35:43 +0930

    Fixing preferences
    You nearly had it all right.  You just do not understand the lifecycle quite right yet.  onCreate is called by Android just after the constructor is called.  Apart from setting default values of properties and configuring the view nothing goes in here.  Use onResume or onStart for these sort of facilities.  I personally use onResume for most of that sort of thing.
    
    Signed-off-by: Damien Cooke <damien@smartphonedev.com>

--- src/com/visualrecruit/powerdevice/LoginActivity.java
+++ src/com/visualrecruit/powerdevice/LoginActivity.java
@@ -13,9 +13,10 @@ public class LoginActivity extends Activity {
     private EditText usernameEditText;
     private EditText passwordEditText;
     private Button okButton;
-    private String _username;
-    private String _password;
+    private String userName;
+    private String passWord;
     public static final String PREFS_NAME = "PowerDevices.dat";
+
     /**
      * Called when the activity is first created.
      */
@@ -24,18 +25,7 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Restore preferences
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        _username = settings.getString("username", "");
-        _password = settings.getString("password", "");
 
-        if(_username != "" && _password != "")
-        {
-            Intent activity = new Intent(LoginActivity.this, BrowserActivity.class);
-            activity.putExtra("username", usernameEditText.getText().toString());
-            activity.putExtra("password", passwordEditText.getText().toString());
-            startActivity(activity);
-        }
     }
 
     @Override
@@ -45,15 +35,21 @@ public class LoginActivity extends Activity {
         usernameEditText = (EditText) findViewById(R.id.user_name);
         passwordEditText = (EditText) findViewById(R.id.password);
 
-        // We need an Editor object to make preference changes.
-        // All objects are from android.context.Context
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString("username", usernameEditText.getText().toString());
-        editor.putString("password", passwordEditText.getText().toString());
 
-        // Commit the edits!
-        editor.commit();
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        userName = settings.getString("username", "");
+        passWord = settings.getString("password", "");
+
+        if((userName != null) && (userName.length() >0 ) && (passWord != null) && (passWord.length() > 0))
+        {
+            Intent activity = new Intent(LoginActivity.this, BrowserActivity.class);
+            activity.putExtra("username", userName);
+            activity.putExtra("password", passWord);
+            startActivity(activity);
+        }
+
+
 
         okButton = (Button)findViewById(R.id.ok_buton);
         okButton.setOnClickListener(new View.OnClickListener() {
@@ -61,6 +57,14 @@ public class LoginActivity extends Activity {
             public void onClick(View v) {
                 Intent activity = new Intent(LoginActivity.this, BrowserActivity.class);
                 //activity.putExtra(Intent.EXTRA_TEXT, "");
+
+                // We need an Editor object to make preference changes.
+                // All objects are from android.context.Context
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("username", usernameEditText.getText().toString());
+                editor.putString("password", passwordEditText.getText().toString());
+                editor.commit(); //this actuallly stores the details
 
                 activity.putExtra("username", usernameEditText.getText().toString());
                 activity.putExtra("password", passwordEditText.getText().toString());
