1771f90773d45f5a5c0fc46e48b26f4f85175771
Shaun O'Reilly <prosoftnz@gmail.com>
2014-07-30 11:31:36 +0930

    new chanages

--- src/com/visualrecruit/powerdevice/LoginActivity.java
+++ src/com/visualrecruit/powerdevice/LoginActivity.java
@@ -23,19 +23,6 @@ public class LoginActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        // Restore preferences
-        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        _username = settings.getString("username", "");
-        _password = settings.getString("password", "");
-
-        if(_username != "" && _password != "")
-        {
-            Intent activity = new Intent(LoginActivity.this, BrowserActivity.class);
-            activity.putExtra("username", usernameEditText.getText().toString());
-            activity.putExtra("password", passwordEditText.getText().toString());
-            startActivity(activity);
-        }
     }
 
     @Override
@@ -45,15 +32,20 @@ public class LoginActivity extends Activity {
         usernameEditText = (EditText) findViewById(R.id.user_name);
         passwordEditText = (EditText) findViewById(R.id.password);
 
-        // We need an Editor object to make preference changes.
-        // All objects are from android.context.Context
+        // Restore preferences
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putString("username", usernameEditText.getText().toString());
-        editor.putString("password", passwordEditText.getText().toString());
+        _username = settings.getString("username", "");
+        _password = settings.getString("password", "");
 
-        // Commit the edits!
-        editor.commit();
+        if((_username != null) && (_username.length() >0 ) && (_password != null) && (_password.length() > 0))
+        {
+            Intent activity = new Intent(LoginActivity.this, BrowserActivity.class);
+            activity.putExtra("username", usernameEditText.getText().toString());
+            activity.putExtra("password", passwordEditText.getText().toString());
+            startActivity(activity);
+        }
+
+
 
         okButton = (Button)findViewById(R.id.ok_buton);
         okButton.setOnClickListener(new View.OnClickListener() {
@@ -61,6 +53,16 @@ public class LoginActivity extends Activity {
             public void onClick(View v) {
                 Intent activity = new Intent(LoginActivity.this, BrowserActivity.class);
                 //activity.putExtra(Intent.EXTRA_TEXT, "");
+
+                // We need an Editor object to make preference changes.
+                // All objects are from android.context.Context
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString("username", usernameEditText.getText().toString());
+                editor.putString("password", passwordEditText.getText().toString());
+
+                // Commit the edits!
+                editor.commit();
 
                 activity.putExtra("username", usernameEditText.getText().toString());
                 activity.putExtra("password", passwordEditText.getText().toString());
